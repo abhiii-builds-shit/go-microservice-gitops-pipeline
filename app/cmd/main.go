6503071ae2c6cbcc132d1a71001e1cb2cbd51563
abhiii-builds-shit <ay96052003@gmail.com>
2026-05-19 22:35:00 +0530

    add routes for status checks

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
+	"os"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 )
+
+var ready bool = false
 
 func main() {
 	app := fiber.New()
 
+	// simulate startup delay
+	go func() {
+		time.Sleep(15 * time.Second)
+		ready = true
+	}()
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
-			"service": "feature-flag-service",
-			"status":  "running",
+			"message": "Go API Running",
 		})
 	})
 
 	app.Get("/health", func(c *fiber.Ctx) error {
-		return c.SendString("OK")
+		return c.SendStatus(200)
+	})
+
+	app.Get("/ready", func(c *fiber.Ctx) error {
+		if !ready {
+			return c.Status(503).SendString("Not Ready")
+		}
+
+		return c.SendString("Ready")
+	})
+
+	app.Get("/crash", func(c *fiber.Ctx) error {
+		os.Exit(1)
+		return nil
 	})
 
 	app.Listen(":5001")
